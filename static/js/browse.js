4892b2657c8b38f57dc0bb59d9588b41fc2ac3a2
muyrety <mykolasuselis@protonmail.com>
2024-09-17 11:37:31 +0300

    Added some API functionality

--- static/js/browse.js
+++ static/js/browse.js
@@ -10,4 +10,33 @@ document.addEventListener("DOMContentLoaded", function () {
         db_button.removeAttribute("disabled");
         user_button.setAttribute("disabled", "");
     });
+
+    loadQuestions();
 });
+
+async function loadQuestions() {
+    try {
+        const session_response = await fetch("https://opentdb.com/api_token.php?command=request");
+        var session = await session_response.json();
+        if (session.response_code != 0)
+        {
+            throw new Error("Bad response code:" + session.response_code);
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    try {
+        const question_response = await fetch("https://opentdb.com/api.php?amount=50&token=" + session.token);
+        const questions_json = await question_response.json();
+        if (questions_json.response_code != 0)
+        {
+            throw new Error("Bad response code:" + questions_json.response_code);
+        }
+        var questions = questions_json.results;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
