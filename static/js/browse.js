20329eca0706750cb4184cc51585d58c97b987f0
Mykolas Uselis <mykolasuselis@protonmail.com>
2024-09-20 20:59:23 +0300

    Added user facing error messages for unsuccessful API calls

--- static/js/browse.js
+++ static/js/browse.js
@@ -1,7 +1,10 @@
-var token_g;
+let token_g;
 
 document.addEventListener("DOMContentLoaded", async function () {
     token_g = await getSessionToken();
+    if (!token_g) {
+        alert("Something went wrong while contacting the API. Try refreshing the page");
+    }
 
     const db_button = document.getElementById("db_button");
     const user_button = document.getElementById("user_button");
@@ -24,18 +27,23 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 async function expandTable(tbl_body) {
 
+    const rate_limit_code = "5"; 
     // Get data from the API
     try {
         const question_response = await fetch("https://opentdb.com/api.php?amount=50&token=" + token_g);
         const questions_json = await question_response.json();
         if (questions_json.response_code != 0)
         {
-            throw new Error("Bad response code:" + questions_json.response_code);
+            throw new Error("Bad response code to question request:" + questions_json.response_code);
         }
         var questions = questions_json.results;
     }
     catch (error) {
+        if (error.message === "Bad response code to question request:" + rate_limit_code) {
+            alert("You have made too many question requests. Please wait at least 5 seconds before loading more questions.");
+        }
         console.error(error);
+        return tbl_body;
     }
 
     // Add data to the table
@@ -79,12 +87,13 @@ async function getSessionToken() {
         const session = await session_response.json();
         if (session.response_code != 0)
         {
-            throw new Error("Bad response code:" + session.response_code);
+            throw new Error("Bad response code to token request:" + session.response_code);
         }
         return session.token;
     }
     catch (error) {
         console.error(error);
+        return false;
     }
 }
 
