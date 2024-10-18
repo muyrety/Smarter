b6ef7f5e4280517a047b98d4782a027f0ed8b4c5
Mykolas Uselis <mykolasuselis@protonmail.com>
2024-10-18 20:42:15 +0300

    minor changes in the schema

--- smarter/schema.sql
+++ smarter/schema.sql
@@ -12,16 +12,17 @@ CREATE TABLE user_questions (
     id INTEGER PRIMARY KEY,
     verified INTEGER DEFAULT 0 NOT NULL CHECK(verified = 0 OR verified = 1),
     type TEXT NOT NULL CHECK(type IN ('boolean', 'multiple')),
-    creator_id TEXT NOT NULL,
+    creator_id INTEGER NOT NULL,
     category INTEGER NOT NULL CHECK(category >= 9 AND category <= 32),
     difficulty TEXT NOT NULL CHECK(difficulty IN ('easy', 'medium', 'hard')),
-    question TEXT NOT NULL UNIQUE
+    question TEXT NOT NULL UNIQUE,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
 );
 
 CREATE TABLE answers (
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
     answer TEXT NOT NULL,
-    correct INTEGER DEFAULT 0 NOT NULL CHECK(correct = 0 OR correct = 1),
+    correct INTEGER NOT NULL CHECK(correct = 0 OR correct = 1),
     FOREIGN KEY (question_id) REFERENCES user_questions(id)
 );
