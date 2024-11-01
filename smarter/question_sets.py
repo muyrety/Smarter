d2a102b729eb795529186119549278075fdb53c0
Mykolas Uselis <mykolasuselis@protonmail.com>
2024-11-01 21:23:59 +0200

    Added user question selection

--- smarter/question_sets.py
+++ smarter/question_sets.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .db import get_db
+from .constants import categories
+from better_profanity import profanity
+from .auth import login_required
 
 bp = Blueprint("question_sets", __name__, url_prefix="/question-sets")
 
@@ -6,6 +10,40 @@ bp = Blueprint("question_sets", __name__, url_prefix="/question-sets")
 def browse():
     return render_template("question-sets/browse.html")
 
-@bp.route("/add")
+@bp.route("/add", methods = ["GET", "POST"])
+@login_required()
 def add():
-    return render_template("question-sets/add.html")
+    if request.method == "GET":
+        return render_template("question-sets/add.html")
+
+    name = request.form.get("name")
+    error = None
+    if not name:
+        error = "Name is required"
+    elif profanity.contains_profanity(name):
+        error = "Question set name contains profanity"
+
+    if error is not None:
+        flash(error, "danger")
+        return render_template("question-sets/add.html")
+
+    return redirect(url_for("question_sets.add_user_generated", name=name, temp=request.form.get("temp")))
+
+@bp.route("/add/user-generated")
+@login_required()
+def add_user_generated():
+    questions = get_db().execute(
+        """SELECT q.id, q.category, q.difficulty, q.question, u.username AS creator
+        FROM questions AS q JOIN users AS u ON u.id = q.creator_id WHERE q.source = 'user'"""
+    ).fetchall()
+
+    # Replace category numbers with strings
+    for question in questions:
+        question["category"] = categories[question["category"]]
+
+    return render_template("question-sets/add_user_questions.html", questions=questions)
+
+@bp.route("/add/opentdb")
+@login_required()
+def add_opentdb():
+    return render_template("question-sets/add_otdb_questions.html")
---
+++ smarter/static/js/select_user_questions.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const parameters = new URLSearchParams(window.location.search);
+    // If this page is loaded just after adding a question_set, load the neccessary values
+    if (parameters.get("name")) {
+        sessionStorage.clear();
+        sessionStorage.setItem("name", parameters.get("name"));
+        sessionStorage.setItem("user_question_ids", JSON.stringify([]));
+        if (parameters.get("temp") === null) {
+            sessionStorage.setItem("temporary", JSON.stringify(false));
+        }
+        else {
+            sessionStorage.setItem("temporary", JSON.stringify(true));
+        }
+    }
+
+    ids = JSON.parse(sessionStorage.getItem("user_question_ids"));
+    disableSelected(ids);
+
+    const forms = document.getElementsByClassName("selectQuestion");
+    Array.from(forms).forEach(function(form) {
+        form.addEventListener("submit", function(e) {
+            e.preventDefault();
+            ids.push(form.elements.id.value);
+            sessionStorage.setItem("user_question_ids", JSON.stringify(ids));
+            form.elements.submitButton.setAttribute("disabled", "");
+        });
+    });
+
+    function disableSelected(ids) {
+        const table = document.getElementById("questionTableBody");
+        const id_col = 0;
+        const form_col = 5;
+        for (const row of table.children) {
+            if (row.children[id_col].textContent in ids) {
+                // Select the rows form and disable it's submit button
+                row.children[form_col].elements.submitButton.setAttribute("disabled", "");
+            }
+        }
+    }
+
+});
