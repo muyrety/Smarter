8d927fc6702801ea43d7fd7b1b0c926957576c54
Mykolas Uselis <mykolasuselis@protonmail.com>
2025-04-17 17:40:27 +0300

    Added a config option for showing a beta alert

--- smarter/__init__.py
+++ smarter/__init__.py
@@ -14,7 +14,8 @@ app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_mapping(
     SECRET_KEY="dev",
-    DATABASE=os.path.join(app.instance_path, "smarter.sqlite")
+    DATABASE=os.path.join(app.instance_path, "smarter.sqlite"),
+    BETA_VERSION=False
 )
 
 app.config.from_pyfile("config.py", silent=True)
